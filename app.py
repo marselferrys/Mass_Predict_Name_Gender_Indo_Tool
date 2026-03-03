08548706cb3a0f84d781ed066d1e90c7e17b4511
121140195_Marchel Ferry Timoteus S <66813668+marselferrys@users.noreply.github.com>
2026-03-03 19:29:44 +0700

    Update app.py

--- app.py
+++ app.py
@@ -169,7 +169,9 @@ if uploaded_file is not None:
                             autopct='%1.1f%%', 
                             startangle=90, 
                             colors=colors,
-                            textprops={'fontsize': 10}
+                            textprops={'fontsize': 10
+                                      'fontweight': 'bold'
+                                      }
                         )
                         ax.axis('equal') 
                         st.pyplot(fig)
